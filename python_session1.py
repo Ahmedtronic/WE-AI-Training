9cfd3437198809f1cdd60643d758c6a5868a1b48
Ahmed Hafez <ahmedtronic@gmail.com>
2026-08-08 13:15:44 +0300

    adding extra files

---
+++ python_session1.py
@@ -0,0 +1,36 @@
+print("Ahmed")
+print("WE Training")
+
+print("Ahmed")
+print("WE Training")
+print("Ahmed")
+print("WE Training")
+print("Ahmed")
+print("WE Training")
+print("Ahmed")
+print("WE Training")
+
+print("Ahmed")
+print("WE Training")
+print("Ahmed")
+print("WE Training")
+
+
+print("Ahmed")
+print("WE Training")
+print("Ahmed")
+print("WE Training")
+print("Ahmed")
+print("WE Training")
+
+print("Ahmed")
+print("WE Training")
+print("WE Training")
+print("Ahmed")
+print("WE Training")
+print("Ahmed")
+print("WE Training")
+
+
+print("Ahmed")
+print("WE Training")
